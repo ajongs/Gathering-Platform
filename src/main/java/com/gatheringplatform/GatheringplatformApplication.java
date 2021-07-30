026ed93886b33253ca09fb9082647368cf8e015d
ajongs <35219960+ajongs@users.noreply.github.com>
2021-07-30 21:49:53 +0900

    first commit

---
+++ src/main/java/com/gatheringplatform/GatheringplatformApplication.java
@@ -0,0 +1,13 @@
+package com.gatheringplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatheringplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatheringplatformApplication.class, args);
+    }
+
+}
