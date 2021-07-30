81c6a8f9b97a10c4ba10c9546c83e2c891c4a49b
ajongs <35219960+ajongs@users.noreply.github.com>
2021-07-31 00:34:30 +0900

    cors 필터 추가

---
+++ src/main/java/com/gatheringplatform/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.gatheringplatform.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+    //cors 설정
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(true)
+        ;
+    }
+}
