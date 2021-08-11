30102e1903d2598993e97384577c94256f40298f
ajongs <35219960+ajongs@users.noreply.github.com>
2021-08-11 11:48:56 +0900

    스웨거 설정 추가

---
+++ src/main/java/com/gatheringplatform/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.gatheringplatform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("com.gatheringplatform.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(Arrays.asList(apiKey()));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("GatheringPlatform API")
+            //.version("버전 작성")
+            //.description("설명 작성")
+            //.license("라이센스 작성")
+            //.licenseUrl("라이센스 URL 작성")
+            .build();
+    }
+    private ApiKey apiKey(){
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+}
--- src/main/java/com/gatheringplatform/controller/UserController.java
+++ src/main/java/com/gatheringplatform/controller/UserController.java
@@ -6,6 +6,8 @@ import com.gatheringplatform.annotation.ValidationGroups;
 import com.gatheringplatform.domain.User;
 import com.gatheringplatform.format.DefaultResponse;
 import com.gatheringplatform.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -32,21 +34,26 @@ public class UserController {
     private UserService userService;
     //login
     @PostMapping(value="/logIn")
+    @ApiOperation(value = "로그인", notes = "로그인 api입니다.\n 로그인이 완료되면 access token과 refresh token이 발급됩니다. \n* Id와 Pw만 입력하시면 됩니다. *")
     public ResponseEntity login(@RequestBody @Validated(ValidationGroups.logIn.class) User user, HttpServletResponse response){
         return new ResponseEntity(userService.logIn(user), HttpStatus.OK);
     }
     //sign-up
     @PostMapping(value="/signUp")
+    @ApiOperation(value = "회원가입", notes = "회원가입을 하는 api입니다.")
     public ResponseEntity signUp(@RequestBody @Validated(ValidationGroups.signUp.class) User user){
         userService.signUp(user);
         return new ResponseEntity(new DefaultResponse("회원가입이 완료되었습니다.", HttpStatus.OK), HttpStatus.OK);
     }
     @Auth
     @PostMapping(value = "/check")
+    @ApiOperation(value = "토큰을 사용해서 api호출이 되는지 확인", notes = "access token을 이용하여 로그인이 필요한 api가 잘 호출되는 지 확인하는 api입니다.",
+    authorizations = @Authorization(value = "JWT"))
     public ResponseEntity check(){
         return new ResponseEntity(new DefaultResponse("@Auth 잘됌", HttpStatus.OK), HttpStatus.OK);
     }
     @PostMapping(value = "/refresh")
+    @ApiOperation(value = "토큰 재발급", notes = "refresh 토큰을 이용하여 access token을 재발급 받는 api입니다.\n만약 refresh 토큰의 유효기간이 7일미만 이라면 refresh 토큰도 함께 발급됩니다.")
     public ResponseEntity refresh(){
         return new ResponseEntity(userService.refresh(), HttpStatus.OK);
     }
